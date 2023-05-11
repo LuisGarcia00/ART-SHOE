3c5ca5ee316764453a15c7d0c73ec2f6fcd7904b
LuisGarcia00 <luisegarcia1899@gmail.com>
2023-05-11 14:09:54 -0600

    luis
    xd

--- src/app/content/modelos/modelos.component.ts
+++ src/app/content/modelos/modelos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-modelos',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./modelos.component.css']
 })
 export class ModelosComponent {
+usuario_activo!:boolean
 
+form={
+  compra:""
 }
+}
+
+
